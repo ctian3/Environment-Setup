3fb1b6e4d02cdde903933a1ee4e289d8c5c2078f
ctian3 <ctian3@hawk.iit.edu>
2015-10-04 17:39:40 -0500

    Update install-webserver.sh

--- install-webserver.sh
+++ install-webserver.sh
@@ -3,7 +3,7 @@
 sudo apt-get update -y
 sudo apt-get install -y apache2 git
 
-git clone https://github.com/ctian3/Application-Setup.git
+git clone https://github.com/ctian3/Environment-Setup.git
 
 mv ./Application-Setup/images /var/www/html/images
 mv ./Application-Setip/index.html /var/www/html
