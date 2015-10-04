85e96f4e752da255504cce7b5305b19ce600066c
Canyuan Tian <ctian3@hawk.iit.edu>
2015-10-04 18:42:10 -0500

    change file name to install-env.sh

---
+++ install-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y apache2 git
+
+git clone https://github.com/ctian3/Application-Setup.git
+
+mv ./Application-Setup/images /var/www/html/images
+mv ./Application-Setup/index.html /var/www/html
+
+echo "Done" > /tmp/hello.txt
