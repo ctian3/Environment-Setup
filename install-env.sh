d193a3a1c0eeaf1a9ac138977b2477a150d38a87
Canyuan Tian <ctian3@hawk.iit.edu>
2015-10-04 19:50:42 -0500

    add mv page2.html command

--- install-env.sh
+++ install-env.sh
@@ -7,5 +7,6 @@ git clone https://github.com/ctian3/Application-Setup.git
 
 mv ./Application-Setup/images /var/www/html/images
 mv ./Application-Setup/index.html /var/www/html
+mv ./Application-Setup/page2.html /var/www/html
 
 echo "Done" > /tmp/hello.txt
